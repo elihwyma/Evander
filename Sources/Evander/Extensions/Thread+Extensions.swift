b3acb19786af61199f63606806d627ef48b40c32
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-10-02 12:29:48 +0100

    Add Thread+Extensios

---
+++ Sources/Evander/Extensions/Thread+Extensions.swift
@@ -0,0 +1,20 @@
+//  Created by Andromeda on 02/10/2021.
+//
+
+import Foundation
+
+
+public extension Thread {
+
+    var threadName: String {
+        if let currentOperationQueue = OperationQueue.current?.name {
+            return "OperationQueue: \(currentOperationQueue)"
+        } else if let underlyingDispatchQueue = OperationQueue.current?.underlyingQueue?.label {
+            return "DispatchQueue: \(underlyingDispatchQueue)"
+        } else {
+            let name = __dispatch_queue_get_label(nil)
+            return String(cString: name, encoding: .utf8) ?? Thread.current.description
+        }
+    }
+    
+}
