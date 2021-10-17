8c100a5dfc95fa44c12aef5a278fcc9688e187d4
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-10-17 17:56:39 +0100

    Declare everything as public

--- Sources/Evander/Extensions/Number++.swift
+++ Sources/Evander/Extensions/Number++.swift
@@ -8,241 +8,241 @@ postfix operator --
 
 
 // Increment
-@discardableResult prefix func ++( x: inout Int) -> Int {
+@discardableResult public prefix func ++( x: inout Int) -> Int {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Int) -> Int {
+@discardableResult public postfix func ++( x: inout Int) -> Int {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout UInt) -> UInt {
+@discardableResult public prefix func ++( x: inout UInt) -> UInt {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout UInt) -> UInt {
+@discardableResult public postfix func ++( x: inout UInt) -> UInt {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout Int8) -> Int8 {
+@discardableResult public prefix func ++( x: inout Int8) -> Int8 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Int8) -> Int8 {
+@discardableResult public postfix func ++( x: inout Int8) -> Int8 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout UInt8) -> UInt8 {
+@discardableResult public prefix func ++( x: inout UInt8) -> UInt8 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout UInt8) -> UInt8 {
+@discardableResult public postfix func ++( x: inout UInt8) -> UInt8 {
     x += 1
     return (x - 1)
 }
-@discardableResult prefix func ++( x: inout Int16) -> Int16 {
+@discardableResult public prefix func ++( x: inout Int16) -> Int16 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Int16) -> Int16 {
-    x += 1
-    return (x - 1)
-}
-
-@discardableResult prefix func ++( x: inout UInt16) -> UInt16 {
-    x += 1
-    return x
-}
-
-@discardableResult postfix func ++( x: inout UInt16) -> UInt16 {
+@discardableResult public postfix func ++( x: inout Int16) -> Int16 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout Int32) -> Int32 {
+@discardableResult public prefix func ++( x: inout UInt16) -> UInt16 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Int32) -> Int32 {
+@discardableResult public postfix func ++( x: inout UInt16) -> UInt16 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout UInt32) -> UInt32 {
+@discardableResult public prefix func ++( x: inout Int32) -> Int32 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout UInt32) -> UInt32 {
+@discardableResult public postfix func ++( x: inout Int32) -> Int32 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout Int64) -> Int64 {
+@discardableResult public prefix func ++( x: inout UInt32) -> UInt32 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Int64) -> Int64 {
+@discardableResult public postfix func ++( x: inout UInt32) -> UInt32 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout UInt64) -> UInt64 {
+@discardableResult public prefix func ++( x: inout Int64) -> Int64 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout UInt64) -> UInt64 {
+@discardableResult public postfix func ++( x: inout Int64) -> Int64 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout Double) -> Double {
+@discardableResult public prefix func ++( x: inout UInt64) -> UInt64 {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Double) -> Double {
+@discardableResult public postfix func ++( x: inout UInt64) -> UInt64 {
     x += 1
     return (x - 1)
 }
 
-@discardableResult prefix func ++( x: inout Float) -> Float {
+@discardableResult public prefix func ++( x: inout Double) -> Double {
     x += 1
     return x
 }
 
-@discardableResult postfix func ++( x: inout Float) -> Float {
+@discardableResult public postfix func ++( x: inout Double) -> Double {
+    x += 1
+    return (x - 1)
+}
+
+@discardableResult public prefix func ++( x: inout Float) -> Float {
+    x += 1
+    return x
+}
+
+@discardableResult public postfix func ++( x: inout Float) -> Float {
     x += 1
     return (x - 1)
 }
 
 // Decrement
-@discardableResult prefix func --( x: inout Int) -> Int {
+@discardableResult public prefix func --( x: inout Int) -> Int {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Int) -> Int {
+@discardableResult public postfix func --( x: inout Int) -> Int {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout UInt) -> UInt {
+@discardableResult public prefix func --( x: inout UInt) -> UInt {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout UInt) -> UInt {
+@discardableResult public postfix func --( x: inout UInt) -> UInt {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout Int8) -> Int8 {
+@discardableResult public prefix func --( x: inout Int8) -> Int8 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Int8) -> Int8 {
+@discardableResult public postfix func --( x: inout Int8) -> Int8 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout UInt8) -> UInt8 {
+@discardableResult public prefix func --( x: inout UInt8) -> UInt8 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout UInt8) -> UInt8 {
+@discardableResult public postfix func --( x: inout UInt8) -> UInt8 {
     x -= 1
     return (x + 1)
 }
-@discardableResult prefix func --( x: inout Int16) -> Int16 {
+@discardableResult public prefix func --( x: inout Int16) -> Int16 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Int16) -> Int16 {
-    x -= 1
-    return (x + 1)
-}
-
-@discardableResult prefix func --( x: inout UInt16) -> UInt16 {
-    x -= 1
-    return x
-}
-
-@discardableResult postfix func --( x: inout UInt16) -> UInt16 {
+@discardableResult public postfix func --( x: inout Int16) -> Int16 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout Int32) -> Int32 {
+@discardableResult public prefix func --( x: inout UInt16) -> UInt16 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Int32) -> Int32 {
+@discardableResult public postfix func --( x: inout UInt16) -> UInt16 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout UInt32) -> UInt32 {
+@discardableResult public prefix func --( x: inout Int32) -> Int32 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout UInt32) -> UInt32 {
+@discardableResult public postfix func --( x: inout Int32) -> Int32 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout Int64) -> Int64 {
+@discardableResult public prefix func --( x: inout UInt32) -> UInt32 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Int64) -> Int64 {
+@discardableResult public postfix func --( x: inout UInt32) -> UInt32 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout UInt64) -> UInt64 {
+@discardableResult public prefix func --( x: inout Int64) -> Int64 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout UInt64) -> UInt64 {
+@discardableResult public postfix func --( x: inout Int64) -> Int64 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout Double) -> Double {
+@discardableResult public prefix func --( x: inout UInt64) -> UInt64 {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Double) -> Double {
+@discardableResult public postfix func --( x: inout UInt64) -> UInt64 {
     x -= 1
     return (x + 1)
 }
 
-@discardableResult prefix func --( x: inout Float) -> Float {
+@discardableResult public prefix func --( x: inout Double) -> Double {
     x -= 1
     return x
 }
 
-@discardableResult postfix func --( x: inout Float) -> Float {
+@discardableResult public postfix func --( x: inout Double) -> Double {
+    x -= 1
+    return (x + 1)
+}
+
+@discardableResult public prefix func --( x: inout Float) -> Float {
+    x -= 1
+    return x
+}
+
+@discardableResult public postfix func --( x: inout Float) -> Float {
     x -= 1
     return (x + 1)
 }
