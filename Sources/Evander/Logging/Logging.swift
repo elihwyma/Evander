130cbbdec42848ea00ed42366c50a8afa099a7ab
Amy While <26681721+elihwyma@users.noreply.github.com>
2022-01-08 02:13:07 +0000

    [Logging] Replace EVLog with print override

--- Sources/Evander/Logging/Logging.swift
+++ Sources/Evander/Logging/Logging.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
-public func EVLog(_ log: Any, separator: String = " ", terminator: String = "\n", file: String = #fileID, lineNumber: Int = #line, function: String = #function) {
-    print("[\(file)/\(lineNumber)/\(function)] \(log)", separator: separator, terminator: terminator)
+public func print(_ items: String..., filename: String = #fileID, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
+    let formatted = "\(filename) [#\(line)] \(function) -> "
+    let output = items.map { "\($0)" }.joined(separator: separator)
+    Swift.print(formatted + output, terminator: terminator)
+}
+
+public func print(_ items: Any..., filename: String = #fileID, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
+    let formatted = "\(filename) [#\(line)] \(function) -> "
+    let output = items.map { "\($0)" }.joined(separator: separator)
+    Swift.print(formatted + output, terminator: terminator)
 }
