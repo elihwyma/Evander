e6233fdb7d7ef07a80911ed7fb76d6285d9264eb
Amy While <26681721+elihwyma@users.noreply.github.com>
2022-01-08 02:02:20 +0000

    [Logging] Add EVLog

---
+++ Sources/Evander/Logging/Logging.swift
@@ -0,0 +1,12 @@
+//
+//  Logging.swift
+//  
+//
+//  Created by Amy While on 08/01/2022.
+//
+
+import Foundation
+
+func EVLog(_ log: Any, separator: String = " ", terminator: String = "\n", file: String = #fileID, lineNumber: Int = #line, function: String = #function) {
+    print("[\(file)/\(lineNumber)/\(function)] \(log)", separator: separator, terminator: terminator)
+}
