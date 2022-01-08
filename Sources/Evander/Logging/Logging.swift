39f3d4401396cd84271de7ed43a86722f8600caa
Amy While <26681721+elihwyma@users.noreply.github.com>
2022-01-08 02:24:52 +0000

    [Logging] It should be perfect now

--- Sources/Evander/Logging/Logging.swift
+++ Sources/Evander/Logging/Logging.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 public func print(_ items: String..., filename: String = #fileID, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
-    let formatted = "\(filename) [#\(line) \(function)] ->"
+    let formatted = "\(filename) [#\(line) \(function)] ->\(separator)"
     let output = items.map { "\($0)" }.joined(separator: separator)
     Swift.print(formatted + output, terminator: terminator)
 }
 
 public func print(_ items: Any..., filename: String = #fileID, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
-    let formatted = "\(filename) [#\(line) \(function)] ->"
+    let formatted = "\(filename) [#\(line) \(function)] ->\(separator)"
     let output = items.map { "\($0)" }.joined(separator: separator)
     Swift.print(formatted + output, terminator: terminator)
 }
